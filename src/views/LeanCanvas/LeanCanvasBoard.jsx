84e49b734afdfab70f77ecf9ace3b46bdac26388
Sanjib Roy <sanjibroy0098@gmail.com>
2020-09-15 12:24:57 +0530

    Resolve warnings

--- src/views/LeanCanvas/LeanCanvasBoard.jsx
+++ src/views/LeanCanvas/LeanCanvasBoard.jsx
@@ -2,7 +2,7 @@ import React, { Component } from "react";
 import { connect } from "react-redux";
 import { DragDropContext } from "react-beautiful-dnd";
 import { reorderPoints, togglePresentationMode } from "../../store/action";
-import { Checkbox, Button, Popup } from "semantic-ui-react";
+import { Checkbox } from "semantic-ui-react";
 import CanvasGridUi from "./CanvasGridUi";
 
 class LeanCanvasBoard extends Component {
@@ -49,51 +49,42 @@ class LeanCanvasBoard extends Component {
     return this.reorder(list, sourceIndex, destIndex);
   };
   render() {
-    let { topics } = this.props;
     let { enablePresentationMode, gridView } = this.state;
 
     return (
       <div className="container">
         <div className="board_wrapper">
-
-        <DragDropContext onDragEnd={this.onDragEnd}>
-          <div className="canvas_header">
-            <div className="toggle_btn">
-              <Checkbox
-                toggle
-                checked={enablePresentationMode}
-                onChange={this.handlePresentationMode}
-              />
-              <nobr> Presentation Mode</nobr>
-            </div>
-            <div className="view">
-              <div onClick={this.handleGridView}>
-                <img
-                  src="https://img.icons8.com/fluent-systems-regular/30/000000/activity-grid.png"
-                  alt=""
+          <DragDropContext onDragEnd={this.onDragEnd}>
+            <div className="canvas_header">
+              <div className="toggle_btn">
+                <Checkbox
+                  toggle
+                  checked={enablePresentationMode}
+                  onChange={this.handlePresentationMode}
                 />
+                <nobr> Presentation Mode</nobr>
               </div>
+              <div className="view">
+                <div onClick={this.handleGridView}>
+                  <img
+                    src="https://img.icons8.com/fluent-systems-regular/30/000000/activity-grid.png"
+                    alt=""
+                  />
+                </div>
 
-              <div onClick={this.handleRowView}>
-                <i class="fas fa-bars"></i>
+                <div onClick={this.handleRowView}>
+                  <i className="fas fa-bars"></i>
+                </div>
               </div>
             </div>
-          </div>
 
-          <CanvasGridUi gridView={gridView} />
-        </DragDropContext>
+            <CanvasGridUi gridView={gridView} />
+          </DragDropContext>
         </div>
       </div>
     );
   }
 }
-
-// function viewPopup() {
-//   return(<ul>
-//     <li>Grid</li>
-//     <li>Collumn</li>
-//   </ul>)
-// }
 
 function mapStateToProps({ topics }) {
   return { topics };
